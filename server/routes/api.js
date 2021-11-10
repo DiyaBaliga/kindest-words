1c0ea4363ab8768958f519c7c4edffb610195c79
Mark Castaneda <markcastaneda@pop-os.localdomain>
2021-11-09 17:53:45 -0800

    Message object and routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Todo = require('../models/todo');
+const Message = require('../models/message')
 
 router.get('/todos', (req, res, next) => {
   // This will return all the data, exposing only the id and action field to the client
@@ -23,6 +24,39 @@ router.post('/todos', (req, res, next) => {
 
 router.delete('/todos/:id', (req, res, next) => {
   Todo.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+
+router.get('/message', (req, res, next) => {
+  // This will return all the data, exposing only the id and action field to the client
+  Message.find({}, 'message')
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.get('/message/:id', (req, res, next) => {
+  // This will return all the data, exposing only the id and action field to the client
+  Message.find({ _id: req.params.id }, 'message')
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.post('/message', (req, res, next) => {
+  if (req.body.Message) {
+    Message.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: 'The input field is empty',
+    });
+  }
+});
+
+router.delete('/message/:id', (req, res, next) => {
+  Message.findOneAndDelete({ _id: req.params.id })
     .then((data) => res.json(data))
     .catch(next);
 });
