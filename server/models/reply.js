820b0e30767f7c1874792a75e57bdef84b58ec7e
Mark Castaneda <markcastaneda@pop-os.localdomain>
2021-11-15 14:14:41 -0800

    Finished some reply routes, tested standard get route and post route.

---
+++ server/models/reply.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create schema for Message
+const reply = new Schema({
+    content: {
+        type: String,
+        minlength: 1,
+        maxlength: [250, 'The message is too long!'],
+        required: [true, 'The text field is required.'],
+    },
+    authorID: {
+        type: Schema.Types.ObjectId,
+        required: [true, 'The message text field is required'],
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+
+});
+
+// Create model for Message
+const Reply = mongoose.model('reply', reply);
+
+module.exports = Reply;
