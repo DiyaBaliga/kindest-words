9f9e374b7bd819206ce5226dfa5b4e803f8e4244
Mark Castaneda <markcastaneda@pop-os.localdomain>
2021-11-15 17:21:39 -0800

    Trying to add api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const Todo = require('../models/todo');
-const Request = require('../models/request')
+//const Request = require('../models/request');
+const Message = require('../models/message');
+const Reply = require('../models/reply');
 
-router.get('/todos', (req, res, next) => {
+router.get('/message', (req, res, next) => {
   // This will return all the data, exposing only the id and action field to the client
-  Todo.find({}, 'action')
+  Message.find({}, 'message')
     .then((data) => res.json(data))
     .catch(next);
 });
 
-router.post('/todos', (req, res, next) => {
-  if (req.body.action) {
-    Todo.create(req.body)
+router.get('/message/:id', (req, res, next) => {
+  // This will return all the data, exposing only the id and action field to the client
+  Message.find({ _id: req.params.id }, 'message')
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.post('/message', (req, res, next) => {
+  if (req.body.Message) {
+    Message.create(req.body)
       .then((data) => res.json(data))
       .catch(next);
   } else {
@@ -24,6 +33,34 @@ router.post('/todos', (req, res, next) => {
 
 router.delete('/todos/:id', (req, res, next) => {
   Todo.findOneAndDelete({ _id: req.params.id })
+});
+router.delete('/message/:id', (req, res, next) => {
+  Message.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.get('/reply', (req, res, next) => {
+  // This will return all the data, exposing only the id and action field to the client
+  Reply.find({})
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.post('/reply', (req, res, next) => {
+  if (req.body.content) {
+    Reply.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: 'The input field is empty',
+    });
+  }
+});
+
+router.delete('/reply', (req, res, next) => {
+  Reply.findOneAndDelete({ _id: req.params.id })
     .then((data) => res.json(data))
     .catch(next);
 });
