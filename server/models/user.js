2735a26545b1c27277c963f0abd6e2d631036225
DAWANG423 <dwangnh@gmail.com>
2021-11-21 14:44:34 -0800

    added routes for registering and login

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-    name :{
-        type : String,
-        required : true
-    }
-})
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+      type: String,
+      required: [true, 'The username text field is required'],
+    },
+    password: {
+      type: String,
+      required: [true, 'The password text field is required'],
+    },
+});
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const Todo = require('../models/todo');
-const Request = require('../models/request')
+const User = require('../models/user')
 
-router.get('/todos', (req, res, next) => {
-  // This will return all the data, exposing only the id and action field to the client
-  Todo.find({}, 'action')
-    .then((data) => res.json(data))
-    .catch(next);
-});
-
-router.post('/todos', (req, res, next) => {
-  if (req.body.action) {
-    Todo.create(req.body)
+router.post('/register', (req, res, next) => {
+  if (req.body.username && req.body.password) {
+    User.create(req.body)
       .then((data) => res.json(data))
       .catch(next);
   } else {
     res.json({
-      error: 'The input field is empty',
+      error: 'Missing username/password',
     });
   }
-});
-
-router.delete('/todos/:id', (req, res, next) => {
-  Todo.findOneAndDelete({ _id: req.params.id })
-    .then((data) => res.json(data))
+ });
+  
+ router.post('/login', (req, res, next) => {
+  User.find({ username: req.body.username })
+    .then(user => {
+      if(user[0].password == req.body.password){
+        res.send('logged in');
+      }
+      else{
+        res.send('incorrect password');
+      }
+    })
     .catch(next);
-});
+ });
+ 
+
+
 
 module.exports = router;
