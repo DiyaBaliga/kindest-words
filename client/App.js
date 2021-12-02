2b32b5029992dad027ae48168f82016f5b98caa9
Mark Castaneda <markcastaneda@pop-os.localdomain>
2021-12-01 19:59:57 -0800

    Added play/pause functionality for audio. Could not consolidate into music.js without breaking functionality.

--- client/App.js
+++ client/App.js
@@ -1,37 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator, TouchableOpacity } from 'react-native';
 import RequestsView from './requestsView/requestsView';
-//import * as React from 'react';
-import { Button } from 'react-native';
 import { Audio } from 'expo-av';
-//import { playMusic, playSound } from './music';
+import Constants from 'expo-constants';
+import { Ionicons } from '@expo/vector-icons';
 
-export default class App extends React.Component {
-  state = {
-    isPlaying: false,
-    playbackInstance: null,
-    currentIndex: 0,
-    volume: 1.0,
-    isBuffering: false
+const SampleTrack = require('./music_resources/lofi.mp3');
+
+export default function App() {
+  const [Loaded, SetLoaded] = React.useState(false);
+  const [Loading, SetLoading] = React.useState(false);
+  const music = React.useRef(new Audio.Sound());
+  var isPlayingBool = false;
+
+  React.useEffect(() => {
+    LoadAudio();
+  }, []);
+
+  const getPlayState = async () => {
+    const playState = await music.current.getStatusAsync();
+    return playState;
   }
-  render() {
-    return (
-      <View style={styles.container}>
-        <RequestsView />
-        <StatusBar style="auto" />
+
+  const PlayPauseAudio = async () => {
+    try {
+      const result = await music.current.getStatusAsync();
+      if (result.isLoaded) {
+        if (result.isPlaying === false) {
+          music.current.playAsync();
+        } else {
+          music.current.pauseAsync();
+        }
+      }
+    } catch (error) {}
+  };
+
+  const LoadAudio = async () => {
+    SetLoading(true);
+    const checkLoading = await music.current.getStatusAsync();
+    if (checkLoading.isLoaded === false) {
+      try {
+        const result = await music.current.loadAsync(SampleTrack, {}, true);
+        if (result.isLoaded === false) {
+          SetLoading(false);
+          console.log('Error in Loading Audio');
+        } else {
+          SetLoading(false);
+          SetLoaded(true);
+        }
+      } catch (error) {
+        console.log(error);
+        SetLoading(false);
+      }
+    } else {
+      SetLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.AudioPlayer}>
+        {Loading ? (
+          <ActivityIndicator size={'small'} color={'red'} />
+        ) : (
+          <>
+            {Loaded === false ? (
+              <>
+                <ActivityIndicator />
+                <Text>Loading Song</Text>{' '}
+              </>
+            ) : (
+              <TouchableOpacity style={styles.AudioPlayer} onPress={PlayPauseAudio}>
+                {getPlayState ? (
+                  <Ionicons name="pause-circle-outline" size={48} color="#ff1188"/>
+                ) : (
+                  <Ionicons name="play-circle-outline" size={48} color="ff1188"/>
+                )}
+               
+                
+              </TouchableOpacity>
+            )}
+          </>
+        )}
       </View>
-    )
-  }
+    </View>
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ffffff',
+    padding: 8,
   },
+  AudioPlayer: {
+    bottom: 0,
+    right: 0,
+    alignItems: 'end',
+  }
 });
-
-const playbackObject = new Audio.Sound();
--- client/music_resources/music.js
+++ client/music_resources/music.js
@@ -1,39 +1,4 @@
-import { StyleSheet, TouchableOpacity, View, Image, Touchable } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-<View style={styles.container}>
-    <View style={styles.controls}>
-        <TouchableOpacity style={styles.control} onPress={() => alert('')}>
-          <Ionicons name='ios-skip-backward' size={48} color='#ff1188' />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.control} onPress={() => alert('')}>
-            {this.state.isPlaying ? (
-                <Ionicons name='ios-pause' size={48} color='#ff1188' />
-            ) : (
-                <Ionicons name='ios-play-circle' size={48} color='#ff1188' />
-            )}
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.control} onPress={() => alert('')}>
-            <Ionicons name='ios-skip-forward' size={48} color='#ff1188' />
-        </TouchableOpacity>
-            </View>
-</View>
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    albumCover: {
-        width: 250,
-        height: 250
-    },
-    controls: {
-        flexDirection: 'row'
-    },
-    control: {
-        margin: 20
-    }
-})
